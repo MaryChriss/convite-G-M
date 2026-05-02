d91b572ba8386856867044e32816653e539f15ff
MaryChriss <marychris.crf@gmail.com>
2026-05-02 00:47:41 -0300

    feat: create attendance confirmation page

--- src/app/pages/confirmation/confirmation.ts
+++ src/app/pages/confirmation/confirmation.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { Header } from '../../components/header/header';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-confirmation',
-  imports: [],
+  imports: [Header, FormsModule, CommonModule],
   templateUrl: './confirmation.html',
   styleUrl: './confirmation.css',
 })
 export class Confirmation {
+  searchTerm: string = '';
 
+  guests: string[] = ['Mariana Christina', 'Maria', 'Marcela', 'Mirella Rodrigues'];
+
+  filteredGuests: string[] = [...this.guests];
+
+  filterList() {
+    const value = this.searchTerm.toLowerCase();
+
+    this.filteredGuests = this.guests.filter((guest) => guest.toLowerCase().includes(value));
+  }
 }
