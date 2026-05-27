02b2bbd01464d0605aea3231fc7dd63010c2b074
MaryChriss <marychris.crf@gmail.com>
2026-05-26 22:41:10 -0300

    feat: history page

--- src/app/pages/history/history.ts
+++ src/app/pages/history/history.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { Header } from '../../components/header/header';
 
 @Component({
   selector: 'app-history',
-  imports: [],
+  imports: [Header],
   templateUrl: './history.html',
   styleUrl: './history.css',
 })
-export class History {
-
-}
+export class History {}
