c6ae504cb69a74169db63f56e1d7072488506dbd
MaryChriss <marychris.crf@gmail.com>
2026-05-26 22:28:17 -0300

    feat: photos page

--- src/app/pages/photos/photos.ts
+++ src/app/pages/photos/photos.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Header } from '../../components/header/header';
 
 @Component({
   selector: 'app-photos',
-  imports: [],
+  imports: [Header],
   templateUrl: './photos.html',
   styleUrl: './photos.css',
 })
